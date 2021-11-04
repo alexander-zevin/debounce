575e8499cffea8651f7bf48906424d526295d22f
danieldyachenko <d.dyachenko@madbrains.ru>
2021-11-04 23:56:38 +0300

    test commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const JsonMinimizerPlugin = require('json-minimizer-webpack-plugin');
+
+module.exports = (_, argv) => {
+  const isProductionMode = argv.mode === 'production';
+
+  return {
+    mode: isProductionMode ? 'production' : 'development',
+    entry: {
+      index: './src/index.ts',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      library: '@danieldyachenko/debounce',
+      libraryExport: 'default',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      globalObject: 'this',
+    },
+    resolve: {
+      extensions: ['*', '.ts', '.js', '.json'],
+    },
+    devtool: isProductionMode ? false : 'inline-source-map',
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({ parallel: true }),
+        new JsonMinimizerPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new ESLintPlugin({
+        extensions: ['ts', 'tsx', 'js', 'jsx'],
+      }),
+    ],
+  };
+};
